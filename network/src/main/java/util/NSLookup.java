0150c4d8f96aec302e2c81cc88f97f4e02f4a102
eu2525 <eu2525@khu.ac.kr>
2024-11-27 17:18:38 +0900

    java study - nslookup practice

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -8,12 +8,18 @@ public class NSLookup {
 
 	public static void main(String[] args) {
 		try {
-//			Scanner scanner = new Scanner(System.in);			
-			InetAddress[] inetAddress = InetAddress.getAllByName("www.poscodx.com");
-			for (InetAddress inet : inetAddress) {
-				System.out.println(inet.getHostAddress());
+			Scanner scanner = new Scanner(System.in);		
+			String scanner_str = null;
+			while (true) {
+				scanner_str = scanner.nextLine();
+				if(scanner_str.equals("exit")) {
+					return;
+				}
+				InetAddress[] inetAddress = InetAddress.getAllByName(scanner_str);
+				for (InetAddress inet : inetAddress) {
+					System.out.println(scanner_str + " : " + inet.getHostAddress());
+				}	
 			}
-			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
