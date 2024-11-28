8a2759b7720d94480ad44d95075fdbda981dac3a
eu2525 <eu2525@khu.ac.kr>
2024-11-28 10:51:08 +0900

    TCP Server

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -2,6 +2,7 @@ package test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -17,7 +18,7 @@ public class TCPServer {
 			// 2. 바인딩(Binding)
 			// 		    여기에 넣는 IP는 어떤 ip가 Connect를 할 수 있는지를 적는거임
 			// 		    Socket에 InetSocketAddress[InetAddress(IPAddress) + port]를 바인딩한다.
-			serverSocket.bind(new InetSocketAddress("0.0.0.0", 54544));
+			serverSocket.bind(new InetSocketAddress("0.0.0.0", 54542));
 
 			// 3. accept
 			Socket socket = serverSocket.accept(); // blocking  -> Clinet가 Connect를 할 때까지 대기 
@@ -31,23 +32,30 @@ public class TCPServer {
 				
 				// 4. IO Stream 받아오기
 				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
 				
-				// 5. 데이터 읽기
-				byte[] buffer = new byte[256];
-				int readByteCount = is.read(buffer); //blocking
-				if(readByteCount == -1){
-					System.out.println("[server] closed by client");
-					return;
+				while(true) {
+					// 5. 데이터 읽기
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer); //blocking
+					if(readByteCount == -1){
+						System.out.println("[server] closed by client");
+						break;
+					}
+				
+					String data = new String(buffer, 0, readByteCount, "utf-8");
+					System.out.println("[server] receive: " + data);					
+					
+					// 6. 데이터 쓰기
+					os.write(data.getBytes("utf-8"));
+					
 				}
-			
-				String data = new String(buffer, 0, readByteCount, "utf-8");
-				System.out.println("[server] receive: " + data);
 			
 			} catch (IOException e) {
 				System.out.println("error:" + e);
 			} finally {
 				try {
-					if(socket != null && socket.isClosed() ) {
+					if(socket != null && !socket.isClosed() ) {
 						socket.close();
 					}
 				} catch (IOException e) {
@@ -58,7 +66,7 @@ public class TCPServer {
 			System.out.println("[server] error:" + e);
 		} finally {
 			try {
-				if(serverSocket != null && serverSocket.isClosed() ) {
+				if(serverSocket != null && !serverSocket.isClosed() ) {
 					serverSocket.close();
 				}
 			} catch (IOException e) {
